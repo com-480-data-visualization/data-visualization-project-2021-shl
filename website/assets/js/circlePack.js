d2a9a79674df45aa85e600d088cfe6b1d5ce560e
Loic <34443227+Cabane123@users.noreply.github.com>
2021-06-03 21:18:11 +0200

    Update circlePack.js

--- website/assets/js/circlePack.js
+++ website/assets/js/circlePack.js
@@ -109,7 +109,6 @@ d3.json("https://raw.githubusercontent.com/com-480-data-visualization/data-visua
     .size([diameter - margin, diameter - margin])
     .padding(2);
 	  
-  var allGroup = ['1998', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']
   // add the options to the button
    d3.select("#selectButton")
      .selectAll('myOptions')
